459cde0a8f28e652b04f7dbf3ecc6bd86e3564b9
Mats Skoglund <mats.skoglund@gmail.com>
2015-10-20 10:46:59 +0200

    Many changes

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,29 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/inventorylist', function(req, res, next) {
 
-var sqlite3 = require('sqlite3').verbose();
-var fs = require("fs");
-var file = "inventory.db";
-var exists = fs.existsSync(file);
+	var sqlite3 = require('sqlite3').verbose();
+	var fs = require("fs");
+	var file = "inventory.db";
+	var exists = fs.existsSync(file);
 
-db = new sqlite3.Database(file);
+	db = new sqlite3.Database(file);
 
-db.serialize(function() {
-  if(!exists) {
-    db.run("CREATE TABLE Inventory (boxid INTEGER, name TEXT)");
-    db.run("Insert into Inventory VALUES (1,'Elkablar')");
-  }
-});
+	db.serialize(function() {
+  		if(!exists) {
+    		db.run("CREATE TABLE Inventory (boxid INTEGER, name TEXT)");
+    		db.run("Insert into Inventory VALUES (1,'Elkablar')");
+  		}
+	});
 
 
-db.serialize(function() { 
-  db.all("SELECT boxid, name FROM Inventory", function(err, row) {
-      res.json(row);
-  });
-});
+	db.serialize(function() { 
+  		db.all("SELECT boxid, name FROM Inventory", function(err, row) {
+      		res.json(row);
+  		});
+	});
 });
 
 /*
@@ -48,10 +49,6 @@ router.post('/addbox', function(req, res) {
  * DELETE to deletebox.
  */
 router.delete('/deletebox/:id', function(req, res) {
-	//var sqlite3 = require('sqlite3').verbose();
-	//var file = "inventory.db";
-	//var db = new sqlite3.Database(file);
-	
 	var boxid = req.params.id;
     var sqlstm = "Delete from Inventory where boxid = " + boxid;
 
